84cd91c4d601ccea382d82aea62849b15a16334f
sll <sll14797378688@163.com>
2020-11-03 23:34:14 +0800

    测试

---
+++ src/main/java/com/dj/test/SllTestApplication.java
@@ -0,0 +1,13 @@
+package com.dj.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SllTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SllTestApplication.class, args);
+    }
+
+}
