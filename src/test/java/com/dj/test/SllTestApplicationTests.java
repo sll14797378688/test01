943bd1e9b350e2b87dd137b29b61b6b49ce8d6ec
sll <sll14797378688@163.com>
2020-11-04 12:04:46 +0800

    测试

---
+++ src/test/java/com/dj/test/SllTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SllTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
